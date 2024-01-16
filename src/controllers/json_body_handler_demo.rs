8aae969f839acd32abce3d71c523c2b2d6be7d04
RobinSaini21 <robinsaini2126@gmail.com>
2024-01-16 09:03:11 +0530

    first commit

---
+++ src/controllers/json_body_handler_demo.rs
@@ -0,0 +1,13 @@
+use actix_web::{web, App, HttpServer, Result};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Info {
+    username: String,
+}
+
+/// extract `Info` using serde
+pub async fn index(info: web::Json<Info>) -> Result<String> {
+    Ok(format!("Welcome {}!", info.username))
+}
+
